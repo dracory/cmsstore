1aaf0d8e22dbc40f4e6c424ea4fb95526d0bd91c
lesichkovm <lesichkovm@gmail.com>
2025-02-23 17:39:21 +0000

    added comments

--- store.go
+++ store.go
@@ -11,6 +11,7 @@ import (
 
 // == TYPE ====================================================================
 
+// store represents the core structure for managing CMS data.
 type store struct {
 	blockTableName     string
 	pageTableName      string
@@ -47,8 +48,8 @@ var _ StoreInterface = (*store)(nil) // verify it extends the interface
 
 // PUBLIC METHODS ============================================================
 
-// AutoMigrate auto migrate
-func (store *store) AutoMigrate(context context.Context, opts ...Option) error {
+// AutoMigrate performs automatic database migrations.
+func (store *store) AutoMigrate(ctx context.Context, opts ...Option) error {
 	if store.db == nil {
 		return errors.New("cms store: database is nil")
 	}
@@ -123,7 +124,7 @@ func (store *store) AutoMigrate(context context.Context, opts ...Option) error {
 		}
 
 		if hasTransaction {
-			_, err := transaction.ExecContext(context, sql)
+			_, err := transaction.ExecContext(ctx, sql)
 
 			if err != nil {
 				return err
@@ -131,7 +132,7 @@ func (store *store) AutoMigrate(context context.Context, opts ...Option) error {
 
 			continue
 		} else {
-			_, err := store.db.ExecContext(context, sql)
+			_, err := store.db.ExecContext(ctx, sql)
 
 			if err != nil {
 				return err
@@ -150,59 +151,71 @@ func (store *store) AutoMigrate(context context.Context, opts ...Option) error {
 	return nil
 }
 
-// EnableDebug - enables the debug option
+// EnableDebug enables or disables debug mode.
 func (st *store) EnableDebug(debug bool) {
 	st.debugEnabled = debug
 }
 
+// MenusEnabled checks if menus are enabled.
 func (store *store) MenusEnabled() bool {
 	return store.menusEnabled
 }
 
+// TranslationsEnabled checks if translations are enabled.
 func (store *store) TranslationsEnabled() bool {
 	return store.translationsEnabled
 }
 
+// VersioningEnabled checks if versioning is enabled.
 func (store *store) VersioningEnabled() bool {
 	return store.versioningEnabled
 }
 
+// Shortcodes returns the list of shortcodes.
 func (store *store) Shortcodes() []ShortcodeInterface {
 	return store.shortcodes
 }
 
+// AddShortcode adds a shortcode to the store.
 func (store *store) AddShortcode(shortcode ShortcodeInterface) {
 	store.shortcodes = append(store.shortcodes, shortcode)
 }
 
+// AddShortcodes adds multiple shortcodes to the store.
 func (store *store) AddShortcodes(shortcodes []ShortcodeInterface) {
 	store.shortcodes = append(store.shortcodes, shortcodes...)
 }
 
+// SetShortcodes sets the list of shortcodes.
 func (store *store) SetShortcodes(shortcodes []ShortcodeInterface) {
 	store.shortcodes = shortcodes
 }
 
+// Middlewares returns the list of middlewares.
 func (store *store) Middlewares() []MiddlewareInterface {
 	return store.middlewares
 }
 
+// AddMiddleware adds a middleware to the store.
 func (store *store) AddMiddleware(middleware MiddlewareInterface) {
 	store.middlewares = append(store.middlewares, middleware)
 }
 
+// AddMiddlewares adds multiple middlewares to the store.
 func (store *store) AddMiddlewares(middlewares []MiddlewareInterface) {
 	store.middlewares = append(store.middlewares, middlewares...)
 }
 
+// SetMiddlewares sets the list of middlewares.
 func (store *store) SetMiddlewares(middlewares []MiddlewareInterface) {
 	store.middlewares = middlewares
 }
 
-func (store *store) toQuerableContext(context context.Context) database.QueryableContext {
-	if database.IsQueryableContext(context) {
-		return context.(database.QueryableContext)
+// toQuerableContext converts a context to a queryable context.
+func (store *store) toQuerableContext(ctx context.Context) database.QueryableContext {
+	if database.IsQueryableContext(ctx) {
+		return ctx.(database.QueryableContext)
 	}
 
-	return database.Context(context, store.db)
+	return database.Context(ctx, store.db)
 }
