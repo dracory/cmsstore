24cf3a40663fc2ba1965689807e5a1f01c864ff0
Milan <lesichkovm@gmail.com>
2026-06-19 23:03:19 +0100

    refactor: consolidate interface definitions and rename implementation files
    - Move BlockQueryInterface from block_query_interface.go into block_query.go
    - Move SiteQueryInterface from site_query_interface.go into site_query.go
    - Rename *_implementation.go files to remove _implementation suffix for cleaner naming
    - Rename *_implementation_test.go files to *_test.go following Go conventions
    - Remove commented-out code from BlockQueryInterface definition

--- translation_query.go
+++ translation_query.go
@@ -2,6 +2,78 @@ package cmsstore
 
 import "errors"
 
+type TranslationQueryInterface interface {
+	Validate() error
+
+	Columns() []string
+	HasColumns() bool
+	SetColumns(columns []string) TranslationQueryInterface
+
+	HasCountOnly() bool
+	IsCountOnly() bool
+	SetCountOnly(countOnly bool) TranslationQueryInterface
+
+	HasCreatedAtGte() bool
+	CreatedAtGte() string
+	SetCreatedAtGte(createdAtGte string) TranslationQueryInterface
+
+	HasCreatedAtLte() bool
+	CreatedAtLte() string
+	SetCreatedAtLte(createdAtLte string) TranslationQueryInterface
+
+	HasHandle() bool
+	Handle() string
+	SetHandle(handle string) TranslationQueryInterface
+
+	HasHandleOrID() bool
+	HandleOrID() string
+	SetHandleOrID(handleOrID string) TranslationQueryInterface
+
+	HasID() bool
+	ID() string
+	SetID(id string) TranslationQueryInterface
+
+	HasIDIn() bool
+	IDIn() []string
+	SetIDIn(idIn []string) TranslationQueryInterface
+
+	HasNameLike() bool
+	NameLike() string
+	SetNameLike(nameLike string) TranslationQueryInterface
+
+	HasOffset() bool
+	Offset() int
+	SetOffset(offset int) TranslationQueryInterface
+
+	HasLimit() bool
+	Limit() int
+	SetLimit(limit int) TranslationQueryInterface
+
+	HasSortOrder() bool
+	SortOrder() string
+	SetSortOrder(sortOrder string) TranslationQueryInterface
+
+	HasOrderBy() bool
+	OrderBy() string
+	SetOrderBy(orderBy string) TranslationQueryInterface
+
+	HasSiteID() bool
+	SiteID() string
+	SetSiteID(siteID string) TranslationQueryInterface
+
+	HasSoftDeletedIncluded() bool
+	SoftDeletedIncluded() bool
+	SetSoftDeletedIncluded(includeSoftDeleted bool) TranslationQueryInterface
+
+	HasStatus() bool
+	Status() string
+	SetStatus(status string) TranslationQueryInterface
+
+	HasStatusIn() bool
+	StatusIn() []string
+	SetStatusIn(statusIn []string) TranslationQueryInterface
+}
+
 func TranslationQuery() TranslationQueryInterface {
 	return &translationQuery{
 		properties: make(map[string]interface{}),
